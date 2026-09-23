a8c217e33ceb36e2a5ee6e49c4b3a8e9ebc2c99b
nikzan <nikzanvv@gmail.com>
2026-09-23 13:02:09 +0500

    feat(module_4): page object refactor, basket pages, parametrized promo tests, user registration tests

--- module_4/pages/login_page.py
+++ module_4/pages/login_page.py
@@ -3,6 +3,16 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        email_field.send_keys(email)
+        password1 = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD1)
+        password1.send_keys(password)
+        password2 = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD2)
+        password2.send_keys(password)
+        button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        button.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
